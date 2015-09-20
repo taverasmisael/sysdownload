bbc76043a1fd0397cb77fe3630693ce8f70203d5
Macro-T <macrot29@gmail.com>
2015-09-20 17:14:25 -0400

    Initialize Repository only with necesary files to start the server

---
+++ gulpfile.js
@@ -0,0 +1,131 @@
+/**
+ *
+ *  Web Starter Kit
+ *  Copyright 2014 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+
+'use strict';
+
+// Include Gulp & tools we'll use
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
+var wiredep = require('wiredep').stream;
+
+var AUTOPREFIXER_BROWSERS = [
+    'ie >= 10',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
+];
+
+// Lint JavaScript
+gulp.task('jshint', function() {
+    return gulp.src(['app/**/*.js', 'public/scripts/**/*.js'])
+        .pipe($.livereload())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe($.jshint.reporter('fail'));
+});
+
+// Compile and automatically prefix stylesheets
+gulp.task('styles', function() {
+    // For best performance, don't add Sass partials to `gulp.src`
+    return gulp.src([
+            'public/styles/**/*.scss',
+            'public/styles/**/*.css',
+            'public/styles/**/*.sass'
+        ])
+        .pipe($.sourcemaps.init())
+        .pipe($.changed('public/styles', {
+            extension: '.css'
+        }))
+        .pipe($.sass({
+            precision: 10,
+            onError: console.error.bind(console, 'Sass error:')
+        }))
+        .pipe($.autoprefixer({
+            browsers: AUTOPREFIXER_BROWSERS
+        }))
+        .pipe($.sourcemaps.write())
+        .pipe($.if('*.css', $.csso()))
+        .pipe(gulp.dest('public/styles'))
+        .pipe($.size({
+            title: 'styles'
+        }))
+        .pipe($.livereload());
+});
+// Optimize images
+gulp.task('images', function() {
+    return gulp.src('public/images/**/*')
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('public/images'))
+        .pipe($.size({
+            title: 'images'
+        }))
+        .pipe($.livereload());
+});
+
+// My Own Wiredep Task for Bower Options
+gulp.task('wiredep', function() {
+    gulp.src('public/styles/*.{sass,scss}')
+        .pipe(wiredep({
+            ignorePath: /^(\.\.\/)+/
+        }))
+        .pipe(gulp.dest('public/styles'));
+    gulp.src('app/views/*.jade')
+        .pipe(wiredep({
+            ignorePath: /^(\.\.\/)+/
+        }))
+        .pipe(gulp.dest('app/views'))
+        .pipe($.livereload());
+});
+gulp.task('watch', function() {
+    gulp.watch(['./public/styles/**/*.{sass,css,scss}'], ['styles']);
+    gulp.watch(['app/images/**/*'], ['images']);
+    gulp.watch(['./public/scripts/**/*.js', 'app/**/*.js'], ['jshint']);
+});
+
+gulp.task('develop', function() {
+    $.livereload.listen();
+    $.nodemon({
+        script: 'app.js',
+        ext: 'js coffee jade',
+        stdout: false
+    }).on('readable', function() {
+        this.stdout.on('data', function(chunk) {
+            if (/^Express server listening on port/.test(chunk)) {
+                $.livereload.changed(__dirname);
+            }
+        });
+        this.stdout.pipe(process.stdout);
+        this.stderr.pipe(process.stderr);
+    });
+});
+
+
+
+gulp.task('default', function(cb) {
+    runSequence('styles', ['wiredep', 'jshint', 'watch', 'develop'], cb)
+});
---
+++ readme.md
@@ -0,0 +1,29 @@
+#Sysdownload
+## Un sistema servidor de archivo con basado en MEAN Stack
+
+Esta es una version con super poderes del proyecto de pruebas que fue el [Spartan](http://github.com/taverasmisael/spartan)
+
+## Este software abarcará
+* Subida de Archivos al servidor
+* Filtro de archivos por categoria y por tipo de archivo
+* Descarga de Archivos
+* Ratting por descarga de los archivos
+
+## Que se usará
+### Para el Frontend
+* [SASS][](http://sass-lang.com/) como el lenguaje de estilos predilecto
+* [Materialize](http://materializecss.com) como framework FrontEnd preferido
+* [Include Media ](include-media.com) para mejores medias Queries en SASS
+* [Gulp](http://gulpjs.com/) para la agilizacion y automatizacion de tareas (WebStarterKit version from google)
+
+### Para que sea MEAN Stack
+* [MongoDB](http://www.mongodb.org/) con [mongoose](http://mongoosejs.com/) para nuestra base de datos
+* [Express](http://expressjs.com/) para nuestro servidor
+* [AngularJs](https://angularjs.org/) para crear una SPA (Single Page Application)
+* [NodeJs](nodejs.org) como plataforma principal
+
+
+## Licencia
+Este proyecto está constituido bajo la creencia OpenSource, por lo que está alojado con licencia [Apache V2](http://www.apache.org/licenses/).
+### TL;DR
+Sientase libre de contribuir, reproducir, mejor, pero siempre atribuyendo a los creadores principales
