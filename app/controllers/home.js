7647ab874f641a7bb70404fa7b4bd6bd6e58c6de
Macro-T <macrot29@gmail.com>
2015-09-21 12:22:13 -0400

    Redirects All pages to index. Starting API Model
    En este commit se redirigen todas las peticiones al index para que se
    pueda trabajar tipo API o en modelo API

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,16 +5,7 @@ var express = require('express'),
   Program = mongoose.model('Program');
 
 module.exports = function (app) {
-  app.use('/', router);
-};
-
-router.get('/', function (req, res, next) {
-  Program.find(function (err, Programas) {
-    if (err) {return next(err);}
-    res.render('index', {
-      title: 'SysDownload',
-      subtitle: 'Un metodo inteligente :D',
-      programas: Programas
-    });
+  app.use('*', function (req, res) {
+    res.render('index');
   });
-});
+};
