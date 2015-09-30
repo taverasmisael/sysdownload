9aef5d909218f27ab5e2a2edd2692897b4f20567
Macro-T <macrot29@gmail.com>
2015-09-30 11:01:22 -0400

    Apply Factory, and filter/search into `main.tpl`. Clean `functions.js`
    Este commit es el que agrega la funcionalidad proporcionada por el
    "controller.navbar" y la "factory.programas.Busqueda" a la vista para
    filtrar los programas por nombre directamente.
    
    Tambien limpia el archivo de `functions.js` ya que la funcionalidad
    del limpiar la barra de navegacion le fue delegada directamente a el
    "controller.navbar".

--- public/scripts/controllers/controller.home.js
+++ public/scripts/controllers/controller.home.js
@@ -1,9 +1,9 @@
 (function(){
     'use strict';
     angular.module('SysDownload')
-            .controller('HomeController' ,['$scope', 'Programas', HomeCtrl]);
+            .controller('HomeController' ,['$scope', 'Programas', 'Busqueda', HomeCtrl]);
 
-    function HomeCtrl ($scope, Programas) {
+    function HomeCtrl ($scope, Programas, Busqueda) {
       var vm = this;
       var CARDCOLORS = ['pink-text text-darken-2', 'deep-purple-text text-darken-2', 'blue-text darken-1', 'cyan-text text-darken-2', 'green-text darken-1', 'yellow-text text-darken-3', 'orange-text text-darken-2', 'brown-text text-darken-3', 'blue-grey-text text-darken-3', 'red-text text-darken-2', 'purple-text text-darken-3', 'light-blue-text text-darken-2', 'teal-text text-darken-3'];
 
@@ -15,5 +15,6 @@
           vm.programas[i].cardColor = CARDCOLORS[Math.floor(Math.random() * CARDCOLORS.length)];
         }
       });
+      vm.filter = Busqueda.filter;
     }
 })();
--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -10,18 +10,10 @@
       event.preventDefault();
       searchBox.toggleClass('expanded');
     });
-    searchCleaner.on('click', function (event) {
-      event.preventDefault();
-      if (searchInput.val() === '') {
-        searchTrigger.trigger('click');
-      }
-      searchInput.val('');
-    });
     var cardsInterval = setInterval(function () {
       if ($('.card.program').length) {
         clearInterval(cardsInterval);
         $('.dropdown-button').dropdown();
-        console.log($('.dropdown-button'));
       }
     }, 1000);
 })();
