4ec3d693b930f47c158a83a298eb4820f877db90
Macro-T <macrot29@gmail.com>
2015-10-01 18:24:58 -0400

    Put a preloader. Fix `material_select` and toast in newProgram added
    Aca ya hay un preloader que se mantiene spinneando mientras se va
    cargando la vista, por detras. El preloader es azul y no cambia de color
    
    Tambien en el `controller.admin` se mejora el "toast" que sale al crear
    el programa y se pone la llamada a la creacion del `material_select`
    cuando se inyecta la plantilla. Para ello ha sido movida la llamada
    al controlador "admin" a la `main.tpl` directamente (Ya que tambien en)
    versiones futuras tendremos un modal para editar que sera controlado
    por el mismo controlador (Valga la redundancia)

--- public/scripts/controllers/controller.admin.js
+++ public/scripts/controllers/controller.admin.js
@@ -8,16 +8,22 @@
       vm.crearPrograma = createProgram;
       Programas.getCategories().$promise.then(function (cats) {
         vm.categories = cats;
-        /*jshint camelcase: false */
-        $('select').material_select();
       });
 
       // Funcionalidades del Controlador
       function createProgram (program) {
         var newProgram = {programa: program};
         Programas.create(newProgram).$promise.then(function () {
-          Materialize.toast('Creado ' + newProgram.name, 3500)
+          Materialize.toast('Creado ' + vm.newProgram.name, 3500);
+          vm.newProgram = '';
+          $('#creationModal').closeModal();
         });
       }
+
+      // Funciones internas del Controlador
+      $scope.$on('$includeContentLoaded', function () {
+        /*jshint camelcase: false */
+        $('select').material_select();
+      })
     }
 })();
