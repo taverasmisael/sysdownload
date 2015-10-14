a69f79bcad1a5b329c7a7ca7aa1cb3c284564248
TaverasMisael <macrot29@gmail.com>
2015-10-13 20:39:28 -0400

    Change Upload.upload to service where it belongs
    Ahora la subida de programas esta directamente en el servicio y ya no
    en el controlador. Aunque debemos saber que no se esta almacenando nada
    en la base de datos y solamente se esta devolviendo del servidor el
    valor de true.
    
    En el proximo commit cambiaremos ese comportamiento

--- public/scripts/controllers/controller.admin.js
+++ public/scripts/controllers/controller.admin.js
@@ -1,36 +1,34 @@
-(function(){
+(function() {
     'use strict';
     angular.module('sysDownload')
-                .controller('AdminController', AdminController);
+        .controller('AdminController', AdminController);
 
-    AdminController.$inject = ['$scope', 'Programas', 'Upload'];
-    function AdminController ($scope, Programas, Upload) {
-      var vm = this;
-      vm.crearPrograma = createProgram;
-      // API.getCategories().$promise.then(function (cats) {
-      //   vm.categories = cats;
-      // });
+    AdminController.$inject = ['$scope', 'Programas'];
 
-      // Funcionalidades del Controlador
-      function createProgram (file, program) {
-          Upload.upload({
-            url: '/api/programs',
-            data: {files: file, program }
-          }).then(function (nuevo) {
-            console.log(nuevo.data);
-            Materialize.toast('Creado ' + nuevo.data, 3500);
-          }, function (err) {
-            console.log(err);
-          }, function (event) {
-            var percentage = parseInt(100.0* event.loaded / event.total);
-            console.log('Progress: ' + percentage);
-          });
-      }
+    function AdminController($scope, Programas) {
+        var vm = this;
+        vm.crearPrograma = createProgram;
+        // API.getCategories().$promise.then(function (cats) {
+        //   vm.categories = cats;
+        // });
 
-      // Funciones internas del Controlador
-      $scope.$on('$includeContentLoaded', function () {
-        /*jshint camelcase: false */
-        $('select').material_select();
-      });
+        // Funcionalidades del Controlador
+        function createProgram(file, program) {
+            Programas.create(file, program)
+                .then(function(nuevoPrograma) {
+                    Materialize.toast('Creado ' + nuevoPrograma.data, 3500);
+                },function(err) {
+                    console.log(err);
+                } ,function(event) {
+                    var percentage = parseInt(100.0 * event.loaded / event.total);
+                    console.log('Progress: ' + percentage);
+              });
+        }
+
+        // Funciones internas del Controlador
+        $scope.$on('$includeContentLoaded', function() {
+            /*jshint camelcase: false */
+            $('select').material_select();
+        });
     }
 })();
--- public/scripts/services/service.programas.js
+++ public/scripts/services/service.programas.js
@@ -1,66 +1,72 @@
-(function(){
+(function() {
     'use strict';
-    angular.module('sysDownload').service('Programas',  Programas);
+    angular.module('sysDownload').service('Programas', Programas);
 
-    Programas.$inject = ['$localStorage', '$sessionStorage', 'API'];
+    Programas.$inject = ['$localStorage', '$sessionStorage', 'Upload', 'API'];
 
-    function Programas ($localStorage, $sesionStorage, API) {
-      var lProgramas = $localStorage.programs || _fillLocalPrograms();
-      var ProgramasService = {
-        // Getters
-        all: lProgramas,
-        getAll: getAll,
-        getByCategory: getByCategory,
-        // Setters
-        create: add,
-        update: update,
-        remove: remove
-      };
-
-      return ProgramasService;
-
-
-      // Service Functionallity
-      function getAll () {
-        // body...
-      }
-
-      function getByCategory (catName) {
-        // body...
-      }
-
-      function add (newProgram) {
-        // body...
-      }
-
-      function update (programId, newData) {
-        // body...
-      }
-
-      function remove (programId) {
-        // body...
-      }
-
-
-      // Internal Functions
-      function _fillLocalPrograms () {
-        API.getAll().$promise.then(function (programs) {
-          console.log('Calliing API...');
-          $localStorage.programs = programs;
-          console.log('27 programs: ', programs);
-          return programs;
-        }).catch(errHanddler);
-      }
-
-
-      // Really Private Functions
-      function errHanddler (err) {
-        var error = err || {
-          name: 'Unexpected Error. No name Provider',
-          type: undefined
+    function Programas($localStorage, $sesionStorage, Upload, API) {
+        var lProgramas = $localStorage.programs || _fillLocalPrograms();
+        var ProgramasService = {
+            // Getters
+            all: lProgramas,
+            getAll: getAll,
+            getByCategory: getByCategory,
+            // Setters
+            create: add,
+            update: update,
+            remove: remove
         };
 
-        console.log(error);
-      }
+        return ProgramasService;
+
+
+        // Service Functionallity
+        function getAll() {
+            // body...
+        }
+
+        function getByCategory(catName) {
+            // body...
+        }
+
+        function add(programFile, newProgram) {
+            return Upload.upload({
+                url: '/api/programs',
+                data: {
+                    files: programFile,
+                    newProgram
+                }
+            });
+        }
+
+        function update(programId, newData) {
+            // body...
+        }
+
+        function remove(programId) {
+            // body...
+        }
+
+
+        // Internal Functions
+        function _fillLocalPrograms() {
+            API.getAll().$promise.then(function(programs) {
+                console.log('Calliing API...');
+                $localStorage.programs = programs;
+                console.log('27 programs: ', programs);
+                return programs;
+            }).catch(errHanddler);
+        }
+
+
+        // Really Private Functions
+        function errHanddler(err) {
+            var error = err || {
+                name: 'Unexpected Error. No name Provider',
+                type: undefined
+            };
+
+            console.log(error);
+        }
     }
 })();
