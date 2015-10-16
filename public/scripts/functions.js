eaf1c883aee0ab09b082b3ca66c027c2c64afa98
TaverasMisael <macrot29@gmail.com>
2015-10-16 13:11:34 -0400

    Create server info markup and basic styles
    Este modulo esta pensado para que aparezca la informacion del servidor
    mas basica pero crucial. Como por ejemplo: Espacio en discos duros, uso
    de la base de datos y porximamente OS en el que corre y uso de la memoria
    en tiempo semi-real

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,16 +1,25 @@
 (function(){
     'use strict';
-    var searchBox = $('.search-box'),
-        searchTrigger = $('.search-box a');
+    $(document).ready(loadFunction());
 
-    searchTrigger.on('click', function (event) {
-      event.preventDefault();
-      searchBox.toggleClass('expanded');
-    });
-    var cardsInterval = setInterval(function () {
-      if ($('.card.program').length) {
-        clearInterval(cardsInterval);
-        $('.dropdown-button').dropdown();
-      }
-    }, 1000);
+    function loadFunction () {
+      var searchBox = $('.search-box'),
+          searchTrigger = $('.search-box a'),
+          modalsTriggers = $('.modal-trigger');
+
+      searchTrigger.on('click', function (event) {
+        event.preventDefault();
+        searchBox.toggleClass('expanded');
+      });
+      var cardsInterval = setInterval(function () {
+        if ($('.card.program').length) {
+          clearInterval(cardsInterval);
+          $('.dropdown-button').dropdown();
+        }
+      }, 1000);
+      $('#infoServer').click(function (e) {
+        e.preventDefault();
+        $('#infoModal').openModal();
+      })
+    }
 })();
