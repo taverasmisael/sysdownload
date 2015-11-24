2e9f5d0f412af2ac1df58350726190b01ed79855
TaverasMisael <macrot29@gmail.com>
2015-11-23 21:49:47 -0400

    Makes commented line to serve static html
    Just uncomment the commented lines on the file and comment the line that render the jade view.
    
    You'll need to comment the `set engine` line 17 and 18 at `./config/express.js`

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,10 @@
 'use strict';
+//var config = require('../../config/config.js');
 module.exports = function (app) {
   app.use('*', function (req, res) {
+    //res.sendFile('index.html', { root: path.join(config.static)});
     res.render('index', {
-      title: 'SysDownload',
-      subtitle: 'Servidor Práctico de Archivos'
+      title: 'Spartan'
     });
   });
 };
